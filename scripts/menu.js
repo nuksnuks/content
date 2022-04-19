96629fb9212aa1d4815b5bc82068c262c9010647
nuksnuks <davidwogelius@gmail.com>
2022-04-19 09:03:34 +0200

    MENU VIRKER!

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,7 +1,7 @@
 document.addEventListener("scroll", function(){
   if (window.scrollY == 0){
     document.getElementById('desktop-menu-container').style.backgroundColor = "rgba(255, 255, 255, 1)";
-    document.getElementById('desktop-menu-container').style.position = "absolute";
+    document.getElementById('desktop-menu-container').style.position = "relative";
   }
   else if (window.scrollY > 0){
     document.getElementById('desktop-menu-container').style.backgroundColor = "rgba(255, 255, 255, 0.5)";
